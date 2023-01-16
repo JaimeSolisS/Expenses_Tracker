1e7427f5c19b19e914da6b278781375d59baae44
JaimeSolisS <jaimesolissoto@me.com>
2023-01-15 18:50:15 -0700

    feat update google sheets

--- app.py
+++ app.py
@@ -5,23 +5,52 @@ from google.oauth2 import service_account
 import gspread
 from gsheetsdb import connect
 import plotly.graph_objects as go
+from gspread_pandas import Spread, Client
+
+
+# Create a Google Authentication connection object
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    # scope of the application
-    scopes=["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"],
+                    st.secrets["gcp_service_account"],
+                    # scope of the application
+                    scopes=scope,
 )
 
-conn = connect(credentials=credentials)
-client = gspread.authorize(credentials)
+spreadsheet_name = st.secrets["spreadsheet"]
+worksheet_name = st.secrets["worksheet"]
 
-spreadsheetname = st.secrets["spreadsheet"]
-worksheetname = st.secrets["worksheet"]
+client = Client(scope=scope,creds=credentials)
+spread = Spread(spreadsheet_name,client = client)
 
-# Open the spreadhseet
-data = client.open(spreadsheetname).worksheet("expenses").get_all_records()
-df = pd.DataFrame.from_dict(data)
+spreadsheet = client.open(spreadsheet_name)
 
+# Get the sheet as dataframe
+def load_the_spreadsheet(worksheet_name):
+    worksheet = spreadsheet.worksheet(worksheet_name)
+    df = pd.DataFrame(worksheet.get_all_records())
+    return df
+
+# Update to Sheet
+def update_the_spreadsheet(worksheet_name,dataframe):
+    col = ['a','b', 'c']
+    spread.df_to_sheet(dataframe[col],sheet = worksheet_name,index = False)
+
+new_entry = {'a': ['apple_from_code'],
+            'b' :  ['banada_from_code'], 
+            'c': ["cat_from_code"]} 
+new_entry_df = pd.DataFrame(new_entry)
+
+# Load data from worksheet
+df = load_the_spreadsheet(worksheet_name)
+
+new_df = pd.concat([df, new_entry_df])
+update_the_spreadsheet(worksheet_name,new_df)
+
+df = load_the_spreadsheet(worksheet_name)
 st.dataframe(df)
+
+
+
