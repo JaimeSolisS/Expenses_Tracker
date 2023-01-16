855e0aab219c6ad565a9e0880fbcb37e3e3e1ae3
JaimeSolisS <jaimesolissoto@me.com>
2023-01-15 21:04:13 -0700

    feat entry form

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from gsheetsdb import connect
 import plotly.graph_objects as go
 from gspread_pandas import Spread, Client
 
+import calendar  # Core Python Module
+from datetime import datetime  # Core Python Module
 
+# ------------------- SETTINGS ------------------- #
 # Create a Google Authentication connection object
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
@@ -21,12 +24,15 @@ credentials = service_account.Credentials.from_service_account_info(
 
 spreadsheet_name = st.secrets["spreadsheet"]
 worksheet_name = st.secrets["worksheet"]
-
 client = Client(scope=scope,creds=credentials)
 spread = Spread(spreadsheet_name,client = client)
-
 spreadsheet = client.open(spreadsheet_name)
 
+# --------------- VARIABLES --------------------- #
+categories = ["", "Expense", "Income"]
+subcategories = ["", "Utilities", "Dogs", "Car", "Other Expenses"]
+
+# --------------- FUNCTIONS --------------------- #
 # Get the sheet as dataframe
 def load_the_spreadsheet(worksheet_name):
     worksheet = spreadsheet.worksheet(worksheet_name)
@@ -35,22 +41,39 @@ def load_the_spreadsheet(worksheet_name):
 
 # Update to Sheet
 def update_the_spreadsheet(worksheet_name,dataframe):
-    col = ['a','b', 'c']
+    col = ['Date','Category', 'Subcategory', 'Detail', 'Amount']
     spread.df_to_sheet(dataframe[col],sheet = worksheet_name,index = False)
 
-new_entry = {'a': ['apple_from_code'],
-            'b' :  ['banada_from_code'], 
-            'c': ["cat_from_code"]} 
-new_entry_df = pd.DataFrame(new_entry)
+def create_new_entry(date, category, subcategory, detail, amount):
+    new_entry = {'Date': [date],
+           'Category' :  [category], 
+           'Subcategory' :  [subcategory], 
+           'Detail' :  [detail], 
+           'Amount' :  [amount]} 
+    return pd.DataFrame(new_entry)
 
-# Load data from worksheet
-df = load_the_spreadsheet(worksheet_name)
+# ----------------------------------------------- #
 
-new_df = pd.concat([df, new_entry_df])
-update_the_spreadsheet(worksheet_name,new_df)
 
-df = load_the_spreadsheet(worksheet_name)
-st.dataframe(df)
+st.header(f"Data Entry")
+with st.form("entry_form", clear_on_submit=True):
 
+    date = st.date_input("Date", datetime.today())
+    category = st.selectbox("Category", categories)
+    subcategory = st.selectbox("Subcategory", subcategories)
+    detail = st.text_input("Detail", "")
+    amount = st.number_input("Amount", min_value=0, step=100)
+    
+    "---"
+    submitted = st.form_submit_button("Save Data")
+    if submitted:
+        try:
+            new_record_df = create_new_entry(date, category, subcategory, detail, amount)
+            df = load_the_spreadsheet(worksheet_name)
+            new_df = pd.concat([df, new_record_df])
+            update_the_spreadsheet(worksheet_name,new_df)
+            st.success("Data saved! 😁")
+        except:
+            st.error("Something went wrong! 😔")
 
 
